527c57b7cca850466331edf350f920f7c62a3fc8
sedakorpe <sefa.korpee@gmail.com>
2019-12-19 21:08:38 +0300

    habibe alacak

---
+++ Yeni klasör/WebApplication13/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using WebApplication13.Dto;
+
+namespace WebApplication13.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Ornek([FromBody] OrnekDto ornekDto )
+        {
+            return View(ornekDto);
+        }
+    }
+}
---
+++ Yeni klasör/WebApplication13/Dto/OrnekDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication13.Dto
+{
+    public class OrnekDto
+    {
+        public string Txt { get; set; }
+    }
+}
